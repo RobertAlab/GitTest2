4b6f447bf67a65215c7b3612da65f434c54e0448
Robert\Robert PC <alabrobert@gmail.com>
2017-05-22 13:50:15 +0800

    Added new php file for reference

---
+++ phpFunctionTest.php
@@ -0,0 +1,10 @@
+<?php
+class GitTest
+{
+	public function displayText(name, desc)
+	{
+		echo "Player Name: " . name . "<br>Role: " . desc;
+	}
+}
+GitTest.displayText("GeT_ReKT", "Rifler/AWPer");
+?>
